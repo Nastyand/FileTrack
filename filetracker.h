25d6f3cf629b41f973efae282187b74a3d2f0304
Anastasia <aandreeva1313@mail.ru>
2023-03-16 20:32:48 +0700

    commit4

--- filetracker.h
+++ filetracker.h
@@ -1,14 +1,36 @@
 #ifndef FILETRACKER_H
 #define FILETRACKER_H
 
-#endif // FILETRACKER_H
+#include <QObject>
 
-class FileState
+class StateFile
 {
+public:
+    StateFile(QString);
+    ~StateFile();
+    GetState();
+private:
     QString FileName;
     qint64 size;
     bool isExist;
-}
+};
 
-//test
-//test2
+class FileMonitor:public QObject
+{
+    Q_OBJECT
+    StateFile *objects;
+    QVector<FileState>objects;
+public:
+    // explicit FileMonitor(const QString& FileName, QObject* parent = nullptr);
+    FileMonitor();
+    ~FileMonitor();
+signals:
+    void SizeChanged(qint64 newSize);
+
+private slots:
+    void FileChanged(const QString& fileName);
+
+};
+
+
+#endif // FILETRACKER_H
